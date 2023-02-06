8676951c0617ee2498f48b25c372b98b82dee8b7
dJtrashw <93950414+dJtrashw@users.noreply.github.com>
2023-02-05 22:26:00 -0500

    level exits + cleanup

---
+++ Assets/[Scripts]/SceneManager/LevelExit.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelExit : MonoBehaviour
+{
+    public static int levelsCompleted;
+
+    private void Start()
+    {
+        levelsCompleted = 0;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            levelsCompleted++;
+
+            if (levelsCompleted < 3)
+                SceneManager.LoadScene("ChooseLevel");
+            else
+                SceneManager.LoadScene("");
+        }
+    }
+}
